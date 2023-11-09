2f4f6c5f5aa5cb5b98cf2ffa2dcf46a6cda7a120
Rodrigo Hamada <104478788+rodrigohamada@users.noreply.github.com>
2023-11-09 19:02:53 -0300

    upload files

---
+++ Jogo_da_Forca/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Jogo_da_Forca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPalavra = new System.Windows.Forms.Label();
+            this.lblTema = new System.Windows.Forms.Label();
+            this.lblLetrasTentadas = new System.Windows.Forms.Label();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.lblDigiteaLetra = new System.Windows.Forms.Label();
+            this.btnNovoJogo = new System.Windows.Forms.Button();
+            this.btnTentar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPalavra
+            // 
+            this.lblPalavra.AutoSize = true;
+            this.lblPalavra.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalavra.Location = new System.Drawing.Point(62, 105);
+            this.lblPalavra.Name = "lblPalavra";
+            this.lblPalavra.Size = new System.Drawing.Size(306, 46);
+            this.lblPalavra.TabIndex = 0;
+            this.lblPalavra.Text = "_ _ _ _ _ _ _ _ _";
+            // 
+            // lblTema
+            // 
+            this.lblTema.AutoSize = true;
+            this.lblTema.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTema.Location = new System.Drawing.Point(66, 9);
+            this.lblTema.Name = "lblTema";
+            this.lblTema.Size = new System.Drawing.Size(63, 20);
+            this.lblTema.TabIndex = 1;
+            this.lblTema.Text = "Tema: ";
+            this.lblTema.Click += new System.EventHandler(this.lblTema_Click);
+            // 
+            // lblLetrasTentadas
+            // 
+            this.lblLetrasTentadas.AutoSize = true;
+            this.lblLetrasTentadas.Location = new System.Drawing.Point(66, 173);
+            this.lblLetrasTentadas.Name = "lblLetrasTentadas";
+            this.lblLetrasTentadas.Size = new System.Drawing.Size(133, 20);
+            this.lblLetrasTentadas.TabIndex = 2;
+            this.lblLetrasTentadas.Text = "Letras Tentadas: ";
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(180, 57);
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(118, 26);
+            this.txtLetra.TabIndex = 3;
+            // 
+            // lblDigiteaLetra
+            // 
+            this.lblDigiteaLetra.AutoSize = true;
+            this.lblDigiteaLetra.Location = new System.Drawing.Point(66, 60);
+            this.lblDigiteaLetra.Name = "lblDigiteaLetra";
+            this.lblDigiteaLetra.Size = new System.Drawing.Size(108, 20);
+            this.lblDigiteaLetra.TabIndex = 4;
+            this.lblDigiteaLetra.Text = "Digite a Letra:";
+            // 
+            // btnNovoJogo
+            // 
+            this.btnNovoJogo.Location = new System.Drawing.Point(70, 222);
+            this.btnNovoJogo.Name = "btnNovoJogo";
+            this.btnNovoJogo.Size = new System.Drawing.Size(141, 45);
+            this.btnNovoJogo.TabIndex = 5;
+            this.btnNovoJogo.Text = "Novo Jogo";
+            this.btnNovoJogo.UseVisualStyleBackColor = true;
+            this.btnNovoJogo.Click += new System.EventHandler(this.btnNovoJogo_Click);
+            // 
+            // btnTentar
+            // 
+            this.btnTentar.Location = new System.Drawing.Point(227, 222);
+            this.btnTentar.Name = "btnTentar";
+            this.btnTentar.Size = new System.Drawing.Size(141, 45);
+            this.btnTentar.TabIndex = 6;
+            this.btnTentar.Text = "Tentar";
+            this.btnTentar.UseVisualStyleBackColor = true;
+            this.btnTentar.Click += new System.EventHandler(this.btnTentar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 279);
+            this.Controls.Add(this.btnTentar);
+            this.Controls.Add(this.btnNovoJogo);
+            this.Controls.Add(this.lblDigiteaLetra);
+            this.Controls.Add(this.txtLetra);
+            this.Controls.Add(this.lblLetrasTentadas);
+            this.Controls.Add(this.lblTema);
+            this.Controls.Add(this.lblPalavra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPalavra;
+        private System.Windows.Forms.Label lblTema;
+        private System.Windows.Forms.Label lblLetrasTentadas;
+        private System.Windows.Forms.TextBox txtLetra;
+        private System.Windows.Forms.Label lblDigiteaLetra;
+        private System.Windows.Forms.Button btnNovoJogo;
+        private System.Windows.Forms.Button btnTentar;
+    }
+}
+
---
+++ Jogo_da_Forca/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_Forca
+{
+    public partial class Form1 : Form
+    {
+        string palavraSecreta;
+        string palavraEmConstrucao;
+        int erros;
+        List<char> letrasTentadas;
+
+        List<string> palavras = new List<string>
+        {
+            "PROGRAMACAO", "DESENVOLVIMENTO", "C#",
+            "WINDOWS", "APLICACAO", "INTERFACE",
+        };
+
+        List<string> temas = new List<string>
+        {
+            "Informática", "Tecnologia", "Linguagens de Programação",
+            "Sistemas Operacionais", "Software", "Desenvolvimento",
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NovoJogo()
+        {
+            Random random = new Random();
+            int indicePalavra = random.Next(palavras.Count);
+            palavraSecreta = palavras[indicePalavra].ToUpper();
+            palavraEmConstrucao = new string('_', palavraSecreta.Length);
+            erros = 0;
+            letrasTentadas = new List<char>();
+
+            lblPalavra.Text = palavraEmConstrucao;
+            lblTema.Text = "Tema: " + temas[indicePalavra];
+            lblLetrasTentadas.Text = "Letras Tentadas: ";
+            txtLetra.Text = "";
+        }
+
+        private bool VerificarResultado()
+        {
+            if (erros >= 6)
+            {
+                MessageBox.Show("Você perdeu! A palavra era: " + palavraSecreta);
+                return true;
+            }
+
+            if (!palavraEmConstrucao.Contains('_'))
+            {
+                MessageBox.Show("Parabéns! Você venceu!");
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NovoJogo();
+        }
+
+        private void btnNovoJogo_Click(object sender, EventArgs e)
+        {
+            NovoJogo();
+        }
+
+        private void btnTentar_Click(object sender, EventArgs e)
+        {
+            Control lblPalavra = this.Controls["lblPalavra"];
+
+            char letra = txtLetra.Text.ToUpper()[0];
+
+            if (letrasTentadas.Contains(letra))
+            {
+                MessageBox.Show("Você já tentou essa letra.");
+                return;
+            }
+
+            letrasTentadas.Add(letra);
+            lblLetrasTentadas.Text = "Letras Tentadas: " + string.Join(", ", letrasTentadas);
+
+            if (palavraSecreta.Contains(letra))
+            {
+                for (int i = 0; i < palavraSecreta.Length; i++)
+                {
+                    if (palavraSecreta[i] == letra)
+                    {
+                        lblPalavra.Text = lblPalavra.Text.Remove(i, 1).Insert(i, letra.ToString());
+                    }
+                }
+            }
+            else
+            {
+                erros++;
+            }
+
+            if (VerificarResultado())
+            {
+                NovoJogo();
+            }
+
+            txtLetra.Text = "";
+        }
+
+        private void lblTema_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
